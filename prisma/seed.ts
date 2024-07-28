36b7d57a519d64fb22d400194d5de252d3346674
Vadim <_s_e_t__@ukr.net>
2024-07-28 15:00:55 +0300

    added seed.ts

---
+++ prisma/seed.ts
@@ -0,0 +1,44 @@
+import { prisma } from "./prisma-client";
+import { hashSync } from "bcrypt";
+
+async function up() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: "User Test",
+        email: "user@test.com",
+        password: hashSync("111111", 10),
+        verified: new Date(),
+        role: "USER",
+      },
+      {
+        fullName: "Admin Admin",
+        email: "admin@test.com",
+        password: hashSync("111111", 10),
+        verified: new Date(),
+        role: "ADMIN",
+      },
+    ],
+  });
+}
+
+async function down() {
+    await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+}
+
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main().then(async () => {
+    await prisma.$disconnect()
+}).catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1)
+})
