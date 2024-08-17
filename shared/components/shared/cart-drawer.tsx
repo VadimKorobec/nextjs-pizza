ce9558a4506aec9d0717e2e67f81698caa7842e4
Vadim <_s_e_t__@ukr.net>
2024-08-17 15:59:47 +0300

    added folder cart-item-details

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -7,6 +7,9 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/shared/components/ui/sheet";
+import Link from "next/link";
+import { Button } from "../ui";
+import { ArrowRight } from "lucide-react";
 
 interface Props {
   children: React.ReactNode;
@@ -17,13 +20,29 @@ const CartDrawer = ({ children, className }: Props) => {
   return (
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
-          <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]">
-              <SheetHeader>
-                  <SheetTitle>
-                      In the cart <span className="font-bold">3 items</span>
-                  </SheetTitle>
-              </SheetHeader>
-              <SheetFooter className="-mx-6 bg-white p-8"></SheetFooter>
+      <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]">
+        <SheetHeader>
+          <SheetTitle>
+            In the cart <span className="font-bold">3 items</span>
+          </SheetTitle>
+        </SheetHeader>
+        <SheetFooter className="-mx-6 bg-white p-8">
+          <div className="w-full">
+            <div className="flex mb-4">
+              <span className="flex flex-1 taxt-lg text-neutral-500">
+                Total
+                <div className="flex-1 border-b border-dasher border-b-neutral-200 relative -top-1 mx-2"></div>
+              </span>
+              <span className="font-bold text-lg">500 $</span>
+            </div>
+            <Link href="/cart">
+              <Button type="submit" className="w-full h-12 text-base">
+                Place your order
+                <ArrowRight className="w-5 ml-2" />
+              </Button>
+            </Link>
+          </div>
+        </SheetFooter>
       </SheetContent>
     </Sheet>
   );
