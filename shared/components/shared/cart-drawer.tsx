556f5278006bbcc7546de5f4211cb191297ca7cb
Vadim <_s_e_t__@ukr.net>
2024-08-22 17:51:53 +0300

    added some code

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -2,7 +2,6 @@
 
 import {
   Sheet,
-  SheetClose,
   SheetContent,
   SheetFooter,
   SheetHeader,
@@ -14,6 +13,8 @@ import { Button } from "../ui";
 import { ArrowRight } from "lucide-react";
 import CartDrawerItem from "./cart-drawer-item";
 import { getCartItemDetails } from "@/shared/lib/get-cart-item-details";
+import { useCartStore } from "@/shared/store/cart";
+import { useEffect } from "react";
 
 interface Props {
   children: React.ReactNode;
@@ -21,30 +22,42 @@ interface Props {
 }
 
 const CartDrawer = ({ children, className }: Props) => {
+  const [totalAmount, fetchCartItems, items] = useCartStore((state) => [
+    state.totalAmount,
+    state.fetchCartItems,
+    state.items,
+  ]);
+
+  useEffect(() => {
+    fetchCartItems();
+  }, []);
+
   return (
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
       <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]">
         <SheetHeader>
           <SheetTitle>
-            In the cart <span className="font-bold">3 items</span>
+            In the cart <span className="font-bold">{items.length} items</span>
           </SheetTitle>
         </SheetHeader>
         <div className="flex-1 -mx-6 mt-5 overflow-auto ">
           <div className="mb-2">
-            <CartDrawerItem
-              id={1}
-              imageUrl={
-                "https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp"
-              }
-              details={getCartItemDetails(1, 20, [
-                { name: "Chicken" },
-                { name: "Garlic" },
-              ])}
-              name={"Chorizo fresco"}
-              price={200}
-              quantity={1}
-            />
+            {items.map((item) => (
+              <CartDrawerItem
+                key={item.id}
+                id={item.id}
+                imageUrl={item.imageUrl}
+                details={getCartItemDetails(
+                  item.pizzaType,
+                  item.pizzaSize,
+                  item.ingredients
+                )}
+                name={item.name}
+                price={item.price}
+                quantity={item.quantity}
+              />
+            ))}
           </div>
         </div>
 
@@ -55,7 +68,7 @@ const CartDrawer = ({ children, className }: Props) => {
                 Total
                 <div className="flex-1 border-b border-dasher border-b-neutral-200 relative -top-1 mx-2"></div>
               </span>
-              <span className="font-bold text-lg">500 $</span>
+              <span className="font-bold text-lg">{totalAmount} $</span>
             </div>
             <Link href="/cart">
               <Button type="submit" className="w-full h-12 text-base">
