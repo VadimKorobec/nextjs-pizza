dfbb4d2fe413986a2b1cdfc5d483476edd534602
Vadim <_s_e_t__@ukr.net>
2024-08-28 15:28:11 +0300

    added some code

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -1,6 +1,9 @@
 import { prisma } from "@/prisma/prisma-client";
 import { NextRequest, NextResponse } from "next/server";
 import crypto from "crypto";
+import { findOrCreatecart } from "@/shared/lib/find-or-create-cart";
+import { CreateCartItemValues } from "@/shared/services/dto/cart.dto";
+import { updateCartTotalAmount } from "@/shared/lib/update-cart-total-amount";
 
 export async function GET(req: NextRequest) {
   try {
@@ -50,8 +53,51 @@ export async function POST(req: NextRequest) {
     let token = req.cookies.get("cartToken")?.value;
 
     if (!token) {
-      token = crypto.randomUUID()
+      token = crypto.randomUUID();
     }
+
+    const userCart = await findOrCreatecart(token);
+
+    const data = (await req.json()) as CreateCartItemValues;
+
+    const findCartItem = await prisma.cartItem.findFirst({
+      where: {
+        cartId: userCart.id,
+        productItemId: data.productItemId,
+        ingredients: { every: { id: { in: data.ingredients } } },
+      },
+    });
+
+    if (findCartItem) {
+      await prisma.cartItem.update({
+        where: {
+          id: findCartItem.id,
+        },
+        data: {
+          quantity: findCartItem.quantity + 1,
+        },
+      });
+
+      const updatedUserCart = await updateCartTotalAmount(token);
+
+      
+    }
+
+    await prisma.cartItem.create({
+      data: {
+        cartId: userCart.id,
+        productItemId: data.productItemId,
+        quantity: 1,
+        ingredients: { connect: data.ingredients?.map((id) => id) },
+      },
+    });
+
+    const updatedUserCart = await updateCartTotalAmount(token);
+
+    const resp = NextResponse.json(updatedUserCart);
+    resp.cookies.set("cartToken", token);
+    return resp;
+
   } catch (error) {
     console.log("[CART_POST] Server error", error);
     return NextResponse.json(
